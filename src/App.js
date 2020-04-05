27dd04180a2a22fc00ac721cdba5e896cb36516c
florent <fdrousset@sqli.com>
2020-04-05 23:25:03 +0200

    JSX variables added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h2>Welcome to the Road to learn React</h2>
-    </div>
-  );
+    var person = {
+        firstName: 'Florent',
+        lastName: 'Drousset',
+        age: 31
+    };
+    return (
+        <div className="App">
+            <h2>Hello, my name is {person.firstName} {person.lastName} and I am {person.age} years old.</h2>
+        </div>
+    );
 }
 
 export default App;
